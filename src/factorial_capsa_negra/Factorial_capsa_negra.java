233f84e3440ab99e606eb43e86dc4c3948113c87
Eloy Albiach Forner <eloy.albiach@gmail.com>
2017-01-26 16:00:27 +0100

    modificació

--- src/factorial_capsa_negra/Factorial_capsa_negra.java
+++ src/factorial_capsa_negra/Factorial_capsa_negra.java
@@ -24,11 +24,11 @@ public class Factorial_capsa_negra {
         if(n == 0 || n== 1){
             return 1;
         }
-        return n * factorial(n - 1);
+        return n * factorial(n - 2);
     }
     
     public static void main(String[] args){
         System.out.println(factorial(Integer.parseInt(args[0])));
-        //System.out.println(factorial_bo(Integer.parseInt(args[0])));
+        
     }
 }
